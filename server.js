e8b88a138e368d74a3be3aa01d3850bfb042ee85
bishwajit2403 <mail2bishwajit@gmail.com>
2016-09-29 21:50:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function (req,res){ 
+    res.send('Article one will be requested and will be serve hare');
+});
+
+app.get('/article-twoe', function (req,res){ 
+    res.send('Article Two will be requested and will be serve hare');
+});
+
+app.get('/article-Three', function (req,res){ 
+    res.send('Article Three will be requested and will be serve hare');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
