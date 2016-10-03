04cae2693629ae9504ecee76b643163685aad854
bishwajit2403 <mail2bishwajit@gmail.com>
2016-10-03 11:32:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,14 @@
-console.log('Loaded!');
-
-var element = document.getElementById('main-text');
-element.innerHTML = " New value ";
-
-// move the image
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight () {
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function() {
-    var interval = setInterval(moveRight, 50);
+// Counter code
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function() {
+    
+   // make a request to the counter endpoint
    
+   //capture the respone and store it in a variable
+   
+   //render the variable in the correct spam
+   counter = counter + 1;
+   var span = documnet.getElementById('count');
+   span.innerHTML = counter.toString();
 };
